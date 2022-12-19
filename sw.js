bf6ef9b065fb7c93c53f35ceabe0666ebb8276c2
Patrik Fejda <patrikfejda@gmail.com>
2022-12-19 13:07:44 +0100

    Make PWA

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('fox-store').then((cache) => cache.addAll([
+            '/momec/',
+            '/momec/index.html',
+            '/momec/index.js',
+            '/momec/sound_source/*'
+        ])),
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+});
